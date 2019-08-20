a573476b7d917e7cb8bd00c04e2f002979ad2bad
fmckenna <fmckenna@berkeley.edu>
2019-08-20 11:47:06 -0700

    fmk - updating file to convert mat to json

--- createEVENT/LowRiseTPU/LowRiseTPU.py
+++ createEVENT/LowRiseTPU/LowRiseTPU.py
@@ -1,3 +1,7 @@
+# python code to open the TPU .mat file
+# and put data into a SimCenter JSON file for
+# wind tunnel data
+
 import sys
 import os
 import subprocess
@@ -10,112 +14,79 @@ from pprint import pprint
 
 inputArgs = sys.argv
 
+print ("Number of arguments: %d" % len(sys.argv))
+print ("The arguments are: %s"  %str(sys.argv))
+
 # set filenames
-bimName = sys.argv[2]
-evtName = sys.argv[4]
+matFileIN = sys.argv[1]
+jsonFileOUT = sys.argv[2]
 
 dataDir = os.getcwd()
 scriptDir = os.path.dirname(os.path.realpath(__file__))
 
-def parseDEDM_MatFile(matFileIn, windFileOut):
+def parseTPU_LowRise_MatFile(matFileIn, windFileOutName):
     print("HELLO PROCESSING")
+
+    file = open(windFileOutName,"w");
+    file.write("{\n");
+
     mat_contents = sio.loadmat(matFileIn);
-    dt = mat_contents['dt_ultimate'][0][0];
-    print("dT=%s" % dt)
-    windDirections = mat_contents['wind_directions'];
-    numDirections = windDirections.size;
-    for dirn in range(0, numDirections):
-        # get forces
-        xForcesUltimate = mat_contents['full_scale_force_x_ultimate'][dirn];
-        yForcesUltimate = mat_contents['full_scale_force_y_ultimate'][dirn];
-        # Set number of floors
-        numFloor = 1
-        numSteps = 0
-        if xForcesUltimate.ndim != 1:
-            numFloor = xForcesUltimate.shape[0]
-            numSteps = xForcesUltimate[0].size
+    breadth = mat_contents['Building_breadth'][0][0];
+
+    depth = mat_contents['Building_depth'][0][0];
+    height = mat_contents['Building_height'][0][0];
+    breadth = mat_contents['Building_breadth'][0][0];
+    pitch = mat_contents['Roof_pitch'][0][0];
+    period = mat_contents['Sample_frequency'][0][0];
+    frequency = mat_contents['Sample_period'][0][0];
+    angle = mat_contents['Wind_direction_angle'][0][0];
+    roofType = mat_contents['Roof_type'][0];
+    if (roofType == 'flat roof'):
+        roofType = 'Flat'
+    elif (roofType == 'gable roof'):
+        roofType = 'Gable'    
+
+    file.write("\"roofType\":\"" + roofType + "\",")
+    file.write("\"meanWindSpeed\":%f," % 22.0);
+    file.write("\"depth\":%f," % depth);
+    file.write("\"height\":%f," % height);
+    file.write("\"breadth\":%f," % breadth);
+    file.write("\"pitch\":%f," % pitch);
+    file.write("\"period\":%f," % period);
+    file.write("\"frequency\":%f," % frequency);
+    file.write("\"incidenceAngle\":%f," % angle);
+    file.write("\"tapLocations\": [");
+    locations = mat_contents['Location_of_measured_points'];
+    numLocations = locations.shape[1];
+    for loc in range(0, numLocations):
+        tag = locations[2][loc]
+        xLoc = locations[0][loc]
+        yLoc = locations[1][loc]
+        face = locations[3][loc]
+        
+        if (loc == numLocations-1):
+            file.write("{\"id\":%d,\"xLoc\":%f,\"yLoc\":%f,\"face\":%d}]" % (tag, xLoc, yLoc, face))
         else:
-            numSteps = xForcesUltimate.size
-
-        windFileOutName = windFileOut + "." + str(windDirections[dirn][0]) + ".json";
-        file = open(windFileOutName,"w");
-        file.write("{\n");
-        file.write("\"type\":\"Wind\",\n");
-        file.write("\"name\":\"" + windFileOutName + "\",\n");
-        file.write("\"dT\":" + str(dt) + ",\n");
-        file.write("\"numSteps\":" + str(numSteps) + ",\n");
-        file.write("\"timeSeries\":[\n");
-        for floor in range(1, numFloor+1):
-            floorForces = xForcesUltimate if numFloor is 1 else xForcesUltimate[floor-1]
-            
-            file.write("{\"name\":\"" + str(floor) + "_X\",\n");            
-            file.write("\"type\":\"Value\",\n");            
-            file.write("\"dT\":" + str(dt) + ",\n");      
-            file.write("\"numSteps\":" + str(floorForces.size) + ",\n");
-            file.write("\"data\":[\n");
-            numForces = floorForces.size;
-            
-            for i in range(0, numForces-1):
-                file.write(str(floorForces[i]) + ",")
-            file.write(str(floorForces[numForces-1]))
-            file.write("]\n},\n");
-
-            floorForces = yForcesUltimate if numFloor is 1 else yForcesUltimate[floor-1]            
-            # floorForces = yForcesUltimate[floor-1]
-            file.write("{\"name\":\"" + str(floor) + "_Y\",\n");            
-            file.write("\"type\":\"Value\",\n");            
-            file.write("\"dT\":" + str(dt) + ",\n");            
-            file.write("\"data\":[\n");
-            numForces = floorForces.size;
-
-            for i in range(0, numForces-1):
-                file.write(str(floorForces[i]) + ",")
-            file.write(str(floorForces[numForces-1]))
-            file.write("]}\n");
-
-            if (floor != numFloor):
-                file.write(",\n");
-            
-        file.write("],\n");
-
-        file.write("\"pattern\":[\n");
-        for floor in range(1, numFloor+1):
-            file.write("{\"name\":\"" + str(floor) + "_X\",\n");            
-            file.write("\"timeSeries\":\"" + str(floor) + "_X\",\n");            
-            file.write("\"type\":\"WindFloorLoad\",\n");            
-            file.write("\"floor\":\"" + str(floor) + "\",\n");
-            file.write("\"dof\":1,\n");
-            file.write("\"value\":1.0\n},\n");
-
-            file.write("{\"name\":\"" + str(floor) + "_Y\",\n");            
-            file.write("\"timeSeries\":\"" + str(floor) + "_Y\",\n");            
-            file.write("\"type\":\"WindFloorLoad\",\n");            
-            file.write("\"floor\":\"" + str(floor) + "\",\n");
-            file.write("\"dof\":2,\n");
-            file.write("\"value\":1.0}\n");
-            
-            if (floor != numFloor):
-                file.write(",\n");
-
-        file.write("]\n");
-
-        
-        file.write("}\n");
-        file.close() 
-        
-        # create a json object for the event
-               
-    print (dt)
-    print (windDirections)
+            file.write("{\"id\":%d,\"xLoc\":%f,\"yLoc\":%f,\"face\":%d}," % (tag, xLoc, yLoc, face))
 
 
-if "--getRV" in inputArgs:
-    getDataFromDEDM_HRP = '"{}/DEDM_HRP" --filenameBIM {} --filenameEVENT {} --getRV'.format(scriptDir, bimName, evtName)
-    subprocess.Popen(getDataFromDEDM_HRP, shell=True).wait()
-    print("DONE. NOW PROCESSING RETURN")
-    parseDEDM_MatFile("tmpSimCenterDEDM.mat",evtName)
-    os.remove("tmpSimCenterDEDM.mat")
-else:
-    getDataFromDEDM_HRP = '"{}/DEDM_HRP" --filenameBIM {} --filenameEVENT {}'.format(scriptDir, bimName, evtName)
-    subprocess.Popen(getDataFromDEDM_HRP, shell=True).wait()
+    file.write(",\"pressureCoefficients\": [");
+    coefficients = mat_contents['Wind_pressure_coefficients'];
+    numLocations = coefficients.shape[1];
+    numValues = coefficients.shape[0];
+    for loc in range(0, numLocations):
+        file.write("{\"id\": %d , \"data\":[" % (loc+1))
+        for i in range(0, numValues-1):
+            file.write("%f," % coefficients[i,loc])
+        if (loc != numLocations-1):
+            file.write("%f]}," % coefficients[numValues-1,loc])
+        else:
+            file.write("%f]}]" % coefficients[numValues-1,loc])
+
+    file.write("}")
+    file.close()
+
+if __name__ == '__main__':    
+    parseTPU_LowRise_MatFile(matFileIN,jsonFileOUT)
+
   
