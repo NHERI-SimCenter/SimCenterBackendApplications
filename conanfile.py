95f6ac321fc42697de3391f367bdbcb45c4ad8a2
Michael Gardner <shellshocked2003@gmail.com>
2019-08-29 14:58:07 -0700

    Set up CI on Travis and Appveyor for packaging

--- conanfile.py
+++ conanfile.py
@@ -29,28 +29,9 @@ class simCenterBackendApps(ConanFile):
     def source(self):
        git = tools.Git(folder=self._source_subfolder)
        git.clone("https://github.com/shellshocked2003/SimCenterBackendApplications", "stable/1.0.0")        
-
-    # def build_requirements(self):
-    #     if self.options.shared:
-    #         self.build_requires("mkl-shared/2019.4@simcenter/stable")
-    #         self.build_requires("ipp-shared/2019.4@simcenter/stable")
-    #         self.build_requires("intel-openmp/2019.4@simcenter/stable")
-    #     else:
-    #         self.build_requires("mkl-static/2019.4@simcenter/stable")
-    #         self.build_requires("ipp-static/2019.4@simcenter/stable")
             
     def configure_cmake(self):
         cmake = CMake(self)
-        
-        # put definitions here so that they are re-used in cmake between
-        # build() and package()
-        # if self.options.shared == "True":
-        #     cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
-        #     cmake.definitions["BUILD_STATIC_LIBS"] = "OFF"
-        # else:
-        #     cmake.definitions["BUILD_SHARED_LIBS"] = "OFF"
-        #     cmake.definitions["BUILD_STATIC_LIBS"] = "ON"
-
         cmake.configure(source_folder=self._source_subfolder)
         return cmake
     
@@ -58,49 +39,11 @@ class simCenterBackendApps(ConanFile):
         cmake = self.configure_cmake()
         cmake.build()
 
-        # if self.settings.os == "Macos":
-        #     if self.options.shared:
-        #         with tools.environment_append({"DYLD_LIBRARY_PATH": [os.getcwd() + "/lib"]}):
-        #             self.run("DYLD_LIBRARY_PATH=%s ctest --verbose" % os.environ['DYLD_LIBRARY_PATH'])
-        #     else:
-        #         self.run("ctest --verbose")                
-        # elif self.settings.os == "Windows":
-        #     if self.settings.build_type == "Release":
-        #         self.run("ctest -C Release --verbose")
-        #     else:
-        #         self.run("ctest -C Debug --verbose")
-        # else:
-        #     self.run("ctest --verbose")
-
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy("*.h", dst="include", src=self._source_subfolder)
-        self.copy("*.tcc", dst="include", src=self._source_subfolder)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*", dst="bin", keep_path=False)                
-        self.copy("*.a", dst="lib", keep_path=False)
-
+        cmake = self.configure_cmake()
+        cmake.install()
+        self.copy("*", dst="bin", src=self._source_subfolder + "/applications")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ['include']
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-        # Add to path so shared objects can be found        
-        if self.options.shared:
-            self.env_info.PATH.append(os.path.join(self.package_folder, "lib"))
-            self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))                        
-            self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
-            self.env_info.DYLD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
-
-        # else:
-        #     if self.settings.os == "Linux":
-        #         # linker flags
-        #         if self.settings.compiler == "gcc":
-        #             self.cpp_info.exelinkflags = ["-static-libgcc", "-static-libstdc++", "-lpthread", "-lm", "-ldl"]
-        #         else:
-        #             self.cpp_info.exelinkflags = ["-static-libstdc++", "-lpthread", "-lm", "-ldl"]
-        # # C++ compilation flags
-        # self.cpp_info.cxxflags = ["-DMKL_ILP64", "-m64"]
