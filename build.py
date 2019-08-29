ebdeb78dfd57a170e6f66ca80b15e50caec12f7e
Michael Gardner <shellshocked2003@gmail.com>
2019-08-29 15:02:18 -0700

    Forgot to add build.py...

---
+++ build.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bincrafters import build_template_default
+
+if __name__ == "__main__":   
+    builder = build_template_default.get_builder(build_types=["Release", "Debug"], archs=["x86_64"])
+    builder.run()
