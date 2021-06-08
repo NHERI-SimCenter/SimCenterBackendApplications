10ec164a9a618aa5b80253b49d135b2a55bcb08e
Charles Wang <c_w@berkeley.edu>
2021-06-08 14:19:53 -0700

    bug fixing siteresponse

--- modules/Workflow/CreateWorkflowJobs.py
+++ modules/Workflow/CreateWorkflowJobs.py
@@ -254,10 +254,10 @@ def generate_workflow_tasks_siteresponse(bldg_filter, config_file, out_dir, task
                           f'--filter {filter} && ')
 
             # copy the results from the task for aggregation
-            file_name = f"surface_motions/{subfolder}/EVENT-*.json"
+            file_name = f"surface_motions/*"
             task_list += (f'mkdir -p {out_dir}/results/surface_motions'
                           f'/{subfolder}/ && ')
-            task_list += (f'cp -f {run_dir}/{file_name} {out_dir}/results'
+            task_list += (f'cp -Rf {run_dir}/{file_name} {out_dir}/results'
                               f'/surface_motions/{subfolder}/ && ')
 
             task_list += f"echo 'cmd generated. Currend dir: '$PWD \n"
