4bddd8cd2da9dcd39c8eab458533f92a2dc30faa
fmckenna <fmckenna@berkeley.edu>
2022-02-07 13:00:38 -0800

    fmk: SimCenterUQ, moving files from quoFEM and modifications to work with new UQ interafce

---
+++ modules/performUQ/SimCenterUQ/SimCenterUQ.py
@@ -0,0 +1,115 @@
+# written: UQ team @ SimCenter
+
+# import functions for Python 2.X support
+from __future__ import division, print_function
+import sys
+if sys.version.startswith('2'): 
+    range=xrange
+    string_types = basestring
+else:
+    string_types = str
+
+import shutil
+import json
+import os
+import stat
+import sys
+import platform
+from subprocess import Popen, PIPE
+from pathlib import Path
+import subprocess
+import argparse
+
+def main(args):
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--workflowInput')
+    parser.add_argument('--workflowOutput')    
+    parser.add_argument('--driverFile')
+    parser.add_argument('--runType')
+
+    args,unknowns = parser.parse_known_args()
+
+    inputFile = args.workflowInput
+    runType = args.runType
+    workflowDriver = args.driverFile
+    outputFile = args.workflowOutput
+    
+    f = open(inputFile)
+    data = json.load(f)
+    
+    if runType in ['runningLocal',]:
+        if (sys.platform == 'darwin' or sys.platform == "linux" or sys.platform == "linux2"):
+            # MAC
+            surrogate = 'surrogateBuild.py' 
+            natafExe = 'nataf_gsa'          
+            osType = 'Linux'
+            workflowDriver1 = 'workflowDriver1'
+            python = 'python3'
+
+        else:
+            
+            surrogate = 'surrogateBuild.py'  
+            natafExe = 'nataf_gsa.exe'
+            workflowDriver = workflowDriver + ".bat"            
+            workflowDriver1 = 'workflowDriver1.bat'
+            osType = 'Windows'
+            python = 'python'            
+        
+
+        cwd = os.getcwd()
+        workdir_main = str(Path(cwd).parents[0]) 
+        print('CWD: ' + cwd)
+        print('work_dir: ' + workdir_main)
+        
+        # open the input json file
+        with open(inputFile) as data_file:    
+            data = json.load(data_file)
+
+        uq_data = data["UQ_Method"]
+
+        myScriptDir = os.path.dirname(os.path.realpath(__file__))
+
+        os.chmod(workflowDriver, stat.S_IXUSR | stat.S_IRUSR | stat.S_IXOTH)
+        
+        st = os.stat(workflowDriver)
+        os.chmod(workflowDriver, st.st_mode | stat.S_IEXEC)
+
+        # change dir to the main working dir for the structure
+        os.chdir("../")
+    
+        #    p = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
+        #    for line in p.stdout:
+        #        print(str(line))
+        
+        #    dakotaCommand = "dakota -input dakota.in -output dakota.out -error dakota.err"
+        
+        '''
+        LATER, CHANGE THE LOCATION
+        '''
+        #  
+        
+        if uq_data['uqType'] == 'Train GP Surrogate Model':
+            simCenterUQCommand = '"{}" "{}/{}" "{}" {} {} {} {}'.format(python,myScriptDir,surrogate,workdir_main,inputFile, workflowDriver, osType, runType)
+        elif uq_data['uqType'] == 'Sensitivity Analysis':
+            simCenterUQCommand = '"{}/{}" "{}" {} {} {} {}'.format(myScriptDir,natafExe,workdir_main,inputFile, workflowDriver, osType, runType)
+        elif uq_data['uqType'] == 'Forward Propagation':
+            simCenterUQCommand = '"{}/{}" "{}" {} {} {} {}'.format(myScriptDir,natafExe,workdir_main,inputFile, workflowDriver, osType, runType)
+            
+        print('running SimCenterUQ: ', simCenterUQCommand)
+
+        # subprocess.Popen(simCenterUQCommand, shell=True).wait()
+        
+        try:
+            result = subprocess.check_output(simCenterUQCommand, stderr=subprocess.STDOUT, shell=True)
+            returncode = 0
+            print('DONE SUCESS')
+        except subprocess.CalledProcessError as e:
+            result = e.output
+            returncode = e.returncode
+            print('DONE FAIL')
+
+if __name__ == '__main__':
+
+    main(sys.argv[1:])            
