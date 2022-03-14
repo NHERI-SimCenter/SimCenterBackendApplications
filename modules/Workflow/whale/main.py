6df02a40890c22ae7fca0813488dcacd855bbbde
bsaakash <11618528+bsaakash@users.noreply.github.com>
2022-03-14 16:52:39 -0400

    abs - using try-except block for the reliability analysis since dakotaTab.out is not produced

--- modules/Workflow/whale/main.py
+++ modules/Workflow/whale/main.py
@@ -1596,23 +1596,30 @@ class Workflow(object):
                 os.chdir(self.run_dir)
                 if bldg_id is not None:
                     os.chdir(bldg_id)
-                dakota_out = pd.read_csv(
-                    "dakotaTab.out", sep=r"\s+", header=0, index_col=0
-                )
+                try:
+                    dakota_out = pd.read_csv(
+                        "dakotaTab.out", sep=r"\s+", header=0, index_col=0
+                    )
 
-                # if the DL is coupled with response estimation, we need to sort the results
-                DL_app = self.workflow_apps.get("DL", None)
-                if DL_app is not None:
-                    is_coupled = DL_app.pref.get("coupled_EDP", None)
-                    if is_coupled:
-                        if "eventID" in dakota_out.columns:
-                            events = dakota_out["eventID"].values
-                            events = [int(e.split("x")[-1]) for e in events]
-                            sorter = np.argsort(events)
-                            dakota_out = dakota_out.iloc[sorter, :]
-                            dakota_out.index = np.arange(dakota_out.shape[0])
+                    # if the DL is coupled with response estimation, we need to sort the results
+                    DL_app = self.workflow_apps.get("DL", None)
+                    if DL_app is not None:
+                        is_coupled = DL_app.pref.get("coupled_EDP", None)
+                        if is_coupled:
+                            if "eventID" in dakota_out.columns:
+                                events = dakota_out["eventID"].values
+                                events = [int(e.split("x")[-1]) for e in events]
+                                sorter = np.argsort(events)
+                                dakota_out = dakota_out.iloc[sorter, :]
+                                dakota_out.index = np.arange(dakota_out.shape[0])
 
-                dakota_out.to_csv("response.csv")
+                    dakota_out.to_csv("response.csv")
+
+                except:
+                    log_msg(
+                        "No tabular data file. 'response.csv' file not created.",
+                        prepend_timestamp=False,
+                    )
 
                 log_msg(
                     "Response simulation finished successfully.",
