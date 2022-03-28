3383e5fd89e28724f0a1c68b4828793da66fe3d0
bsaakash <11618528+bsaakash@users.noreply.github.com>
2022-03-28 15:32:09 -0400

    abs - changing name of driver to to driver from workflow_driver

--- modules/performUQ/other/runWorkflowDriver.py
+++ modules/performUQ/other/runWorkflowDriver.py
@@ -11,9 +11,9 @@ def runWorkflow(index):
 
     command2 = "blank"
     if platform == "linux" or platform == "linux2" or platform == "darwin":
-        command2 = os.path.join(os.getcwd(), "workflow_driver")
+        command2 = os.path.join(os.getcwd(), "driver")
     elif platform == "win32":
-        command2 = os.path.join(os.getcwd(), "workflow_driver.bat")
+        command2 = os.path.join(os.getcwd(), "driver.bat")
 
     # os.system(command1)
     os.system(command2)    
