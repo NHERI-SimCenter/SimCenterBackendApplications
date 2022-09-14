e43131a027a44f9f42eca6e00ccbfbf5a71cb349
fmckenna <fmckenna@berkeley.edu>
2022-09-13 23:02:46 -0700

    fmk - updating MDOF-LU, adding uniuts to SAM, modification to work within sWHALE

--- modules/createSAM/MDOF-LU/MDOF-LU.cpp
+++ modules/createSAM/MDOF-LU/MDOF-LU.cpp
@@ -9,13 +9,25 @@
 #include "HazusSAM_Generator.h"
 #include "Building.h"
 #include <cstring>
+#include "whereami.h"
+
 
 int main(int argc, char **argv)
 {
   char *filenameBIM = 0;
   char *filenameEVENT = 0;
   char *filenameSAM = 0;
-  char *filenameHazusData = 0;
+
+  // get path to exe
+  int dirname_length = 0;
+  int length = wai_getExecutablePath(NULL, 0, NULL);
+  char* filenameHazusData = (char*)malloc(length + 20);
+  wai_getExecutablePath(filenameHazusData, length, &dirname_length);
+  strncpy(&filenameHazusData[dirname_length],"/data/HazusData.txt",19);
+  filenameHazusData[dirname_length+19] = '\0';
+
+  //  std::cerr << "My path: " <<  filenameHazusData << "\n";
+  
   double stdStiffness = 0.1;
   double stdDamping = 0.1;
   bool getRV = false;
@@ -23,47 +35,49 @@ int main(int argc, char **argv)
   int arg = 1;
   while (arg < argc) {
     if ((strcmp(argv[arg], "-filenameBIM") ==0) || 
-		(strcmp(argv[arg], "--filenameBIM") ==0)) {
+	(strcmp(argv[arg], "--filenameBIM") ==0)) {
       arg++;
       filenameBIM = argv[arg];
     }
     else if ((strcmp(argv[arg], "-filenameEVENT") == 0) ||
-		     (strcmp(argv[arg], "--filenameEVENT") == 0)) {
+	     (strcmp(argv[arg], "--filenameEVENT") == 0)) {
       arg++;
       filenameEVENT = argv[arg];
     }
     else if ((strcmp(argv[arg], "-filenameSAM") == 0) ||
-		     (strcmp(argv[arg], "--filenameSAM") == 0)) {
+	     (strcmp(argv[arg], "--filenameSAM") == 0)) {
       arg++;
       filenameSAM = argv[arg];
     }
     else if ((strcmp(argv[arg], "-hazusData") == 0) ||
-		     (strcmp(argv[arg], "--hazusData") == 0)) {
+	     (strcmp(argv[arg], "--hazusData") == 0)) {
       arg++;
       filenameHazusData = argv[arg];
     }
     else if ((strcmp(argv[arg], "-stdStiffness") == 0) ||
-         (strcmp(argv[arg], "--stdStiffness") == 0)) {
+	     (strcmp(argv[arg], "--stdStiffness") == 0)) {
       arg++;
       stdStiffness = atof(argv[arg]);
     }
     else if ((strcmp(argv[arg], "-stdDamping") == 0) ||
-         (strcmp(argv[arg], "--stdDamping") == 0)) {
+	     (strcmp(argv[arg], "--stdDamping") == 0)) {
       arg++;
       stdDamping = atof(argv[arg]);
     }
     else if ((strcmp(argv[arg], "-getRV") == 0) ||
-		     (strcmp(argv[arg], "--getRV") == 0)) {
+	     (strcmp(argv[arg], "--getRV") == 0)) {
       getRV = true;
     }
     arg++;
   }
-
-
+  
+  
   HazusSAM_Generator* aim = new HazusSAM_Generator(filenameHazusData);
   Building *theBuilding = new Building();
   
+  
   if(getRV == true) {
+    theBuilding->readBIM(filenameEVENT, filenameBIM);
     theBuilding->writeRV(filenameSAM, stdStiffness, stdDamping);
   } else {
     theBuilding->readBIM(filenameEVENT, filenameBIM, filenameSAM);
