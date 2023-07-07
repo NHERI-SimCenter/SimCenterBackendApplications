ca01b331b8f51806f5945a6c89e2e649961f2b3b
bsaakash <11618528+bsaakash@users.noreply.github.com>
2023-07-06 23:38:13 -0700

    abs - minore code improvements

--- modules/performUQ/UQpy/src/sampling/mcmc/ModifiedMetropolisHastingsDto.py
+++ modules/performUQ/UQpy/src/sampling/mcmc/ModifiedMetropolisHastingsDto.py
@@ -6,8 +6,8 @@ class ModifiedMetropolisHastingsDto(UQpyDTO):
     method: Literal["Modified Metropolis Hastings"] = "Modified Metropolis Hastings"
     burn_length: int = Field(default=0, alias='burn-in', ge=0)
     jump: int = Field(default=1, ge=0)
-    dimension: int = Field(..., gt=0)
-    n_chains: int = Field(default=1, alias='numChains', ge=1)
+    # dimension: int = Field(..., gt=0)
+    # n_chains: int = Field(default=1, alias='numChains', ge=1)
     random_state: int = Field(..., alias='randomState')
     save_log_pdf: bool = False
     concatenate_chains = True
@@ -23,14 +23,15 @@ class ModifiedMetropolisHastingsDto(UQpyDTO):
         stretch_parameters = self.dict()
         stretch_parameters.pop("method")
         stretch_parameters["log_pdf_target"] = f"marginals.log_pdf"
-        stretch_parameters["seed"] = f"list(marginals.rvs({self.n_chains},))"
+        stretch_parameters["seed"] = f"list(marginals.rvs(numRV,))"
+        # stretch_parameters["seed"] = f"list(marginals.rvs({self.n_chains},))"
         str_parameters = str()
         for key in stretch_parameters:
             if stretch_parameters[key] is None: continue
-            str_parameters += key + "=" + str(stretch_parameters[key]) + ","
+            str_parameters += key + "=" + str(stretch_parameters[key]) + ", "
 
         prerequisite_str = import_statement
-        prerequisite_str += "sampling = " + class_name + "(" + str_parameters + ")\n"
+        prerequisite_str += "sampling = " + class_name + "(" + str_parameters + ")"
         sampling_str = "sampling"
 
         return (prerequisite_str, sampling_str)
--- modules/performUQ/UQpy/src/sampling/mcmc/StretchDto.py
+++ modules/performUQ/UQpy/src/sampling/mcmc/StretchDto.py
@@ -12,8 +12,8 @@ class StretchDto(UQpyDTO):
     method: Literal['Stretch'] = 'Stretch'
     burn_length: int = Field(default=0, alias='burn-in', ge=0)
     jump: int = Field(default=1, ge=0)
-    dimension: int = Field(..., gt=0)
-    n_chains: int = Field(default=2, alias='numChains', ge=2)
+    # dimension: int = Field(..., gt=0)
+    # n_chains: int = Field(default=2, alias='numChains', ge=2)
     random_state: int = Field(..., alias='randomState')
     scale: float = Field(..., gt=0)
 
@@ -27,18 +27,20 @@ class StretchDto(UQpyDTO):
         stretch_parameters = self.dict()
         stretch_parameters.pop("method")
         stretch_parameters["log_pdf_target"] = f"marginals.log_pdf"
-        stretch_parameters["seed"] = f"list(marginals.rvs({self.n_chains},))"
+        # stretch_parameters["seed"] = f"list(marginals.rvs({self.n_chains},))"
+        stretch_parameters["seed"] = f"list(marginals.rvs(numRV,))"
         str_parameters = str()
         for key in stretch_parameters:
             if stretch_parameters[key] is None: continue
-            str_parameters += key + "=" + str(stretch_parameters[key]) + ","
+            str_parameters += key + "=" + str(stretch_parameters[key]) + ", "
 
         # prerequisite_str = import_statement + import_likehood_statement
         prerequisite_str = import_statement
-        prerequisite_str += "sampling = " + class_name + "(" + str_parameters + ")\n"
+        prerequisite_str += "sampling = " + class_name + "(" + str_parameters + ")"
         sampling_str = "sampling"
 
         return (prerequisite_str, sampling_str)
 
 
-SamplingMethod = Annotated[Union[StretchDto, ModifiedMetropolisHastingsDto], Field(discriminator='method')]
+# SamplingMethod = Annotated[Union[StretchDto, ModifiedMetropolisHastingsDto], Field(discriminator='method')]
+SamplingMethod = Union[StretchDto, ModifiedMetropolisHastingsDto]
