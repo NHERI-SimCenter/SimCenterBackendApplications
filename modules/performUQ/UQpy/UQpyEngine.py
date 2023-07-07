1ab22f29620bf8f0474e220e2db04fec394b603d
bsaakash <11618528+bsaakash@users.noreply.github.com>
2023-07-06 23:28:28 -0700

    abs - cleaning up UQpyEngine

--- modules/performUQ/UQpy/UQpyEngine.py
+++ modules/performUQ/UQpy/UQpyEngine.py
@@ -9,14 +9,10 @@ if sys.version.startswith('2'):
 else:
     string_types = str
 
-import shutil
-import json
 import os
 import stat
 import sys
 import platform
-from subprocess import Popen, PIPE
-from pathlib import Path
 import subprocess
 import argparse
 import click
@@ -26,12 +22,12 @@ import click
 @click.option("--workflowOutput", required=True, help="Path to JSON file containing the details for post-processing.")
 @click.option("--driverFile", required=True, help="ASCII file containing the details on how to run the FEM application.")
 @click.option("--runType", required=True, type=click.Choice(['runningLocal','runningRemote']))
+
 def main(workflowinput, workflowoutput, driverfile, runtype):
-    # with open(inputFile, "r") as f:
-    #     data = json.load(f)
+
     python = sys.executable
 
-    # run on local computer
+    # get os type
     osType = platform.system()
     if runtype in ['runningLocal',]:
         if (sys.platform == 'darwin' or sys.platform == "linux" or sys.platform == "linux2"):
@@ -42,31 +38,21 @@ def main(workflowinput, workflowoutput, driverfile, runtype):
     elif runtype in ['runningRemote',]:
         osType = 'Linux'        
         
-    cwd = os.getcwd()
-    templateDir = cwd
-    tmpSimCenterDir = str(Path(cwd).parents[0])
+
     thisScriptDir = os.path.dirname(os.path.realpath(__file__))
 
-
     os.chmod("{}/preprocessUQpy.py".format(thisScriptDir),  stat.S_IWUSR | stat.S_IXUSR | stat.S_IRUSR | stat.S_IXOTH)
-    # 1. Create the python script
+    # 1. Create the UQy analysis python script
     preprocessorCommand = "'{}' '{}/preprocessUQpy.py' --workflowInput {} --driverFile {} --runType {} --osType {}".format(python, thisScriptDir,
                                                                         workflowinput,
                                                                         driverfile,
                                                                         runtype,
                                                                         osType)
-    with open("preprocessResult.txt", "w") as f:
-        f.write(preprocessorCommand)
 
-    res = subprocess.Popen(preprocessorCommand, shell=True).wait()
-    with open("preprocessResult.txt", "a") as f:
-        f.write(str(res))
+    subprocess.run(preprocessorCommand, shell=True)
 
-        
     if runtype in ['runningLocal']:
         os.chmod(driverfile,  stat.S_IWUSR | stat.S_IXUSR | stat.S_IRUSR | stat.S_IXOTH)
-    
-    
     
     # 2. Run the python script
     UQpycommand = python + " UQpyAnalysis.py" + " 1> uqpy.log 2>&1 "
@@ -74,22 +60,10 @@ def main(workflowinput, workflowoutput, driverfile, runtype):
     #Change permission of workflow driver
     st = os.stat(driverfile)
     os.chmod(driverfile, st.st_mode | stat.S_IEXEC)
-
-    # # copy the analysis Python script created by UQpy to the main working dir for the structure
-    # shutil.move("UQpyAnalysis.py", "../")
-        
-    # # change dir to the main working dir for the structure
-    # os.chdir("../")
         
     if runtype in ['runningLocal']:
-    
         print('running UQpy: ', UQpycommand)
-        try:
-            result = subprocess.check_output(UQpycommand, stderr=subprocess.STDOUT, shell=True)
-            returncode = 0
-        except subprocess.CalledProcessError as e:
-            result = e.output
-            returncode = e.returncode
+        subprocess.run(UQpycommand, stderr=subprocess.STDOUT, shell=True)
 
 if __name__ == '__main__':
     main()            
