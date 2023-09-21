7853d90c72ecbe877dd0fdc84a27a05d65077c5a
bsaakash <11618528+bsaakash@users.noreply.github.com>
2023-09-21 15:15:32 -0700

    abs - using variable names consistently

--- modules/performUQ/UCSD_UQ/UCSD_UQ.py
+++ modules/performUQ/UCSD_UQ/UCSD_UQ.py
@@ -17,44 +17,41 @@ if __name__ == "__main__":
 
     args, unknowns = parser.parse_known_args()
 
-    inputFile = args.workflowInput
-    outputFile = args.workflowOutput
-    workflowDriver = args.driverFile
+    workflowInput = args.workflowInput
+    workflowOutput = args.workflowOutput
+    driverFile = args.driverFile
     runType = args.runType
 
-    cwd = os.getcwd()
-    templateDir = cwd
-    tmpSimCenterDir = str(Path(cwd).parents[0])
+    templateDir = os.getcwd()
+    tmpSimCenterDir = str(Path(templateDir).parents[0])
     
     mainScriptPath = os.path.dirname(os.path.realpath(__file__))
     
     # Change permission of workflow driver
     if platform.system() != "Windows":
-        workflowDriverFile = os.path.join(templateDir, workflowDriver)
+        workflowDriverFile = os.path.join(templateDir, driverFile)
         if runType in ["runningLocal"]:
             os.chmod(workflowDriverFile, stat.S_IXUSR | stat.S_IRUSR | stat.S_IXOTH)
         st = os.stat(workflowDriverFile)
         os.chmod(workflowDriverFile, st.st_mode | stat.S_IEXEC)
         pythonCommand = "python3"
-        workflowDriver = "./" + workflowDriver
+        driverFile = "./" + driverFile
     else:
         pythonCommand = "python"
 
-    print("WORKFLOW: " + workflowDriver)
+    print("WORKFLOW: " + driverFile)
 
     if runType in ["runningLocal"]:
-        dakotaJsonFile = os.path.join(os.path.abspath(templateDir), inputFile)
+        dakotaJsonFile = os.path.join(os.path.abspath(templateDir), workflowInput)
         with open(dakotaJsonFile, "r") as f:
             jsonInputs = json.load(f)
 
         # Get the path to the mainscript.py of TMCMC
         mainScript = os.path.join(mainScriptPath, "mainscript.py")
-        command = f'"{pythonCommand}" "{mainScript}" "{tmpSimCenterDir}" "{templateDir}" {runType} {workflowDriver} {inputFile}'
+        command = f'"{pythonCommand}" "{mainScript}" "{tmpSimCenterDir}" "{templateDir}" {runType} {driverFile} {workflowInput}'
         print(command)
         try:
-            result = subprocess.check_output(
-                command, stderr=subprocess.STDOUT, shell=True
-            )
+            result = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
             returnCode = 0
         except subprocess.CalledProcessError as e:
             result = e.output
