c375bc09eb18b4fc5209b6beee9c5a4f6dbf0a7b
bsaakash <11618528+bsaakash@users.noreply.github.com>
2023-09-27 07:38:56 -0700

    abs - adding functionality for running jobs in parallel

--- modules/performUQ/common/utils.py
+++ modules/performUQ/common/utils.py
@@ -5,9 +5,12 @@ import subprocess
 import sys
 import os
 import numpy as np
-from typing import Union
+from typing import Union, Optional
 from numpy.typing import NDArray
 import traceback
+from multiprocessing.pool import Pool
+from mpi4py import MPI
+from mpi4py.futures import MPIPoolExecutor
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
@@ -178,3 +181,58 @@ class ModelEval:
             pass
         return outputs
 
+
+class ParallelRunnerMultiprocessing:
+    def __init__(self, run_type="runningLocal") -> None:
+        self.run_type = run_type
+        self.num_processors = self.get_num_processors()
+    
+    def get_num_processors(self) -> int:
+        num_processors = os.cpu_count()
+        if num_processors is None:
+            num_processors = 1
+        if num_processors < 1:
+            raise ValueError(f"Number of processes must be at least 1. Got {num_processors}")
+        return num_processors
+
+    def get_pool(self) -> Pool:
+        self.pool = Pool(processes=self.num_processors)
+        return self.pool
+    
+    def close_pool(self) -> None:
+        self.pool.close()
+
+    def run(self, func, iterable, chunksize: Optional[int]=None) -> list:
+        try:
+            isinstance(self.pool, Pool)
+        except AttributeError:
+            self.pool = self.get_pool()   
+        return self.pool.starmap(func=func, iterable=iterable, chunksize=chunksize)
+
+class ParallelRunnerMPI4PY:
+    def __init__(self, run_type="runningRemote") -> None:
+        self.run_type = run_type
+        self.comm = MPI.COMM_WORLD
+        self.num_processors = self.get_num_processors()
+    
+    def get_num_processors(self) -> int:
+        num_processors = self.comm.Get_size()
+        if num_processors is None:
+            num_processors = 1
+        if num_processors < 1:
+            raise ValueError(f"Number of processes must be at least 1. Got {num_processors}")
+        return num_processors
+    
+    def get_pool(self) -> MPIPoolExecutor:
+        self.pool = MPIPoolExecutor(max_workers=self.num_processors)
+        return self.pool
+    
+    def close_pool(self) -> None:
+        self.pool.shutdown()
+
+    def run(self, func, iterable, chunksize: int=1, unordered: bool=False) -> list:
+        try:
+            isinstance(self.pool, MPIPoolExecutor)
+        except AttributeError:
+            self.pool = self.get_pool()   
+        return list(self.pool.starmap(fn=func, iterable=iterable, chunksize=chunksize, unordered=unordered))
