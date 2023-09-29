5920ed7b062d4c84c877f833a38b7161dcd4a498
fmckenna <fmckenna@berkeley.edu>
2023-09-29 09:12:24 -0700

    fmk - adding CoupledDigitalTwin dummy app

---
+++ modules/createEVENT/coupledDigitalTwin/CoupledDigitalTwin.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 The Regents of the University of California
+#
+# This file is part of the SimCenter Backend Applications.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# You should have received a copy of the BSD 3-Clause License along with
+# SimCenter Backend Applications. If not, see <http://www.opensource.org/licences/>.
+#
+# Contributors:
+# fmk
+
+import argparse
+
+def main(inputFile,
+         evtFile,
+         getRV) :
+
+    print("Finished CoupledDigitalTwin application .. does nothing locally yet!")
+
+if __name__ == '__main__':
+
+	#Defining the command line arguments
+	parser = argparse.ArgumentParser(
+		"Run the CoupledDigitalTwin application.",
+		allow_abbrev=False)
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--filenameAIM', default=None)
+	parser.add_argument('--filenameEVENT', default='NA')
+	parser.add_argument('--getRV', nargs='?', const=True, default=False)
+
+	args = parser.parse_args()        
+
+	main(inputFile = args.filenameAIM,
+			evtFile = args.filenameEVENT,
+			getRV = args.getRV)
+
+         
