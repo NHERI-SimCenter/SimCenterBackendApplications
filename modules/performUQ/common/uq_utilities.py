5d2bc4b20272c5f501372053a071ffedd4c5220d
bsaakash <11618528+bsaakash@users.noreply.github.com>
2023-10-19 10:55:24 -0700

    abs - formatting file and organizig imports

--- modules/performUQ/common/uq_utilities.py
+++ modules/performUQ/common/uq_utilities.py
@@ -3,15 +3,15 @@ import os
 import shutil
 import subprocess
 import sys
-import os
-import numpy as np
-from typing import Union, Optional, Any
-from numpy.typing import NDArray
 import traceback
 from multiprocessing.pool import Pool
+from typing import Any, Optional, Union
+
+import numpy as np
+import quoFEM_RV_models
 from ERAClasses.ERADist import ERADist
 from ERAClasses.ERANataf import ERANataf
-import quoFEM_RV_models 
+from numpy.typing import NDArray
 
 
 def _copytree(src, dst, symlinks=False, ignore=None):
@@ -24,9 +24,11 @@ def _copytree(src, dst, symlinks=False, ignore=None):
             _copytree(s, d, symlinks, ignore)
         else:
             try:
-                if not os.path.exists(d) or \
-                    os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
-                        shutil.copy2(s, d)
+                if (
+                    not os.path.exists(d)
+                    or os.stat(s).st_mtime - os.stat(d).st_mtime > 1
+                ):
+                    shutil.copy2(s, d)
             except Exception as ex:
                 msg = f"Could not copy {s}. \
                     The following error occurred: \n{ex}"
@@ -41,18 +43,19 @@ def _append_msg_in_out_file(msg, out_file_name: str = "ops.out"):
 
         startingCharId = error_FEM.lower().find("error")
 
-        if startingCharId >0:
-            startingCharId = max(0,startingCharId-20)
-            endingID = max(len(error_FEM),startingCharId+200)
+        if startingCharId > 0:
+            startingCharId = max(0, startingCharId - 20)
+            endingID = max(len(error_FEM), startingCharId + 200)
             errmsg = error_FEM[startingCharId:endingID]
-            errmsg=errmsg.split(" ", 1)[1]
-            errmsg=errmsg[0:errmsg.rfind(" ")]
+            errmsg = errmsg.split(" ", 1)[1]
+            errmsg = errmsg[0 : errmsg.rfind(" ")]
             msg += "\n"
             msg += "your model says...\n"
             msg += "........\n" + errmsg + "\n........ \n"
-            msg += "to read more, see " + os.path.join(os.getcwd(), 
-                                                       out_file_name)
-    
+            msg += "to read more, see " + os.path.join(
+                os.getcwd(), out_file_name
+            )
+
     return msg
 
 
@@ -62,16 +65,20 @@ class ModelEvaluationError(Exception):
 
 
 class SimCenterWorkflowDriver:
-    def __init__(self, full_path_of_tmpSimCenter_dir: str, 
-                 list_of_dir_names_to_copy_files_from: list[str], 
-                 list_of_rv_names: list[str], driver_filename: str, 
-                 length_of_results: int,
-                 workdir_prefix: str = "workdir",
-                 ignore_nans: bool = True) -> None:
-        
+    def __init__(
+        self,
+        full_path_of_tmpSimCenter_dir: str,
+        list_of_dir_names_to_copy_files_from: list[str],
+        list_of_rv_names: list[str],
+        driver_filename: str,
+        length_of_results: int,
+        workdir_prefix: str = "workdir",
+        ignore_nans: bool = True,
+    ) -> None:
         self.full_path_of_tmpSimCenter_dir = full_path_of_tmpSimCenter_dir
-        self.list_of_dir_names_to_copy_files_from = \
+        self.list_of_dir_names_to_copy_files_from = (
             list_of_dir_names_to_copy_files_from
+        )
         self.list_of_rv_names = list_of_rv_names
         self.driver_filename = driver_filename
         self.length_of_results = length_of_results
@@ -79,25 +86,26 @@ class SimCenterWorkflowDriver:
         self.ignore_nans = ignore_nans
 
         self.num_rv = len(self.list_of_rv_names)
-    
+
     def _check_size_of_sample(self, sample_values: NDArray) -> None:
         num_samples = len(sample_values)
         if num_samples > 1:
             msg = f"Do one simulation at a time. There were {num_samples} \
                 samples provided in the sample value {sample_values}."
             raise ModelEvaluationError(msg)
-        
+
         for i in range(num_samples):
             num_values_in_each_sample = len(sample_values[i])
             if num_values_in_each_sample != self.num_rv:
                 msg = f"Expected {self.num_rv} values in each sample, found \
                     {num_values_in_each_sample} in {sample_values}."
-                raise ModelEvaluationError(msg)        
-    
+                raise ModelEvaluationError(msg)
+
     def _create_workdir(self, simulation_number: int) -> str:
         workdir = os.path.join(
-            self.full_path_of_tmpSimCenter_dir, 
-            f"{self.workdir_prefix}.{simulation_number + 1}")
+            self.full_path_of_tmpSimCenter_dir,
+            f"{self.workdir_prefix}.{simulation_number + 1}",
+        )
         if os.path.exists(workdir):
             for root, dirs, files in os.walk(workdir):
                 for file in files:
@@ -120,19 +128,22 @@ class SimCenterWorkflowDriver:
             if msg != "0":
                 raise ModelEvaluationError(msg)
         return workdir
-    
-    def _create_params_file(self, sample_values: NDArray, 
-                            workdir: str) -> None:
+
+    def _create_params_file(
+        self, sample_values: NDArray, workdir: str
+    ) -> None:
         list_of_strings_to_write = []
         list_of_strings_to_write.append(f"{self.num_rv}")
         for i, rv in enumerate(self.list_of_rv_names):
             list_of_strings_to_write.append(f"{rv} {sample_values[0][i]}")
-        try: 
+        try:
             with open(os.path.join(workdir, "params.in"), "w") as f:
                 f.write("\n".join(list_of_strings_to_write))
         except Exception as ex:
-            raise ModelEvaluationError(f"Failed to create params.in file in \
-                        {workdir}. The following error occurred: \n{ex}")
+            raise ModelEvaluationError(
+                f"Failed to create params.in file in \
+                        {workdir}. The following error occurred: \n{ex}"
+            )
 
     def _execute_driver_file(self, workdir: str) -> None:
         command = f"{os.path.join(workdir, self.driver_filename)} \
@@ -143,12 +154,14 @@ class SimCenterWorkflowDriver:
             completed_process.check_returncode()
         except subprocess.CalledProcessError as ex:
             returnStringList = ["Failed to run the model."]
-            returnStringList.append(f"The command to run the model was \
-                                    {ex.cmd}")
+            returnStringList.append(
+                f"The command to run the model was \
+                                    {ex.cmd}"
+            )
             returnStringList.append(f"The return code was {ex.returncode}")
             returnStringList.append(f"The following error occurred: \n{ex}")
             raise ModelEvaluationError(f"\n\n".join(returnStringList))
-    
+
     def _read_outputs_from_results_file(self, workdir: str) -> NDArray:
         if glob.glob("results.out"):
             outputs = np.loadtxt("results.out").flatten()
@@ -161,7 +174,7 @@ class SimCenterWorkflowDriver:
             msg = "Error running FEM: 'results.out' is empty\n"
             msg = _append_msg_in_out_file(msg, out_file_name="ops.out")
             raise ModelEvaluationError(msg)
-        
+
         if outputs.shape[0] != self.length_of_results:
             msg = f"Error running FEM: 'results.out' contains \
                 {outputs.shape[0]} values, expected to get \
@@ -176,8 +189,9 @@ class SimCenterWorkflowDriver:
 
         return outputs
 
-    def evaluate_model_once(self, simulation_number: int, \
-                            sample_values: NDArray) -> Union[str, NDArray]:
+    def evaluate_model_once(
+        self, simulation_number: int, sample_values: NDArray
+    ) -> Union[str, NDArray]:
         outputs = ""
         try:
             sample_values = np.atleast_2d(sample_values)
@@ -188,11 +202,13 @@ class SimCenterWorkflowDriver:
             outputs = self._read_outputs_from_results_file(workdir)
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
-            outputs = f"\nSimulation number: {simulation_number}\n" \
-                      + f"Samples values: {sample_values}\n" 
-            outputs += "".join(traceback.format_exception(exc_type, 
-                                                          exc_value, 
-                                                          exc_traceback))
+            outputs = (
+                f"\nSimulation number: {simulation_number}\n"
+                + f"Samples values: {sample_values}\n"
+            )
+            outputs += "".join(
+                traceback.format_exception(exc_type, exc_value, exc_traceback)
+            )
         finally:
             os.chdir(self.full_path_of_tmpSimCenter_dir)
         return outputs
@@ -202,20 +218,22 @@ class ParallelRunnerMultiprocessing:
     def __init__(self, run_type: str = "runningLocal") -> None:
         self.run_type = run_type
         self.num_processors = self.get_num_processors()
-    
+
     def get_num_processors(self) -> int:
         num_processors = os.cpu_count()
         if num_processors is None:
             num_processors = 1
         if num_processors < 1:
-            raise ValueError(f"Number of processes must be at least 1. \
-                             Got {num_processors}")
+            raise ValueError(
+                f"Number of processes must be at least 1. \
+                             Got {num_processors}"
+            )
         return num_processors
 
     def get_pool(self) -> Pool:
         self.pool = Pool(processes=self.num_processors)
         return self.pool
-    
+
     def close_pool(self) -> None:
         self.pool.close()
 
@@ -223,9 +241,10 @@ class ParallelRunnerMultiprocessing:
         try:
             isinstance(self.pool, Pool)
         except AttributeError:
-            self.pool = self.get_pool()   
-        return self.pool.starmap(func=func, iterable=iterable, 
-                                 chunksize=chunksize)
+            self.pool = self.get_pool()
+        return self.pool.starmap(
+            func=func, iterable=iterable, chunksize=chunksize
+        )
 
 
 # def get_parallel_runner_instance(run_type: str):
@@ -235,28 +254,33 @@ class ParallelRunnerMultiprocessing:
 #         return ParallelRunnerMultiprocessing(run_type)
 
 
-# def get_parallel_runner_function(parallel_runner: 
-#                                  Union[ParallelRunnerMultiprocessing, 
+# def get_parallel_runner_function(parallel_runner:
+#                                  Union[ParallelRunnerMultiprocessing,
 #                                        ParallelRunnerMPI4PY]):
 #     return parallel_runner.run
+
 
 def make_ERADist_object(name, opt, val) -> ERADist:
     return ERADist(name=name, opt=opt, val=val)
 
 
 def create_one_marginal_distribution(rv_data) -> ERADist:
-    string = f'quoFEM_RV_models.{rv_data["distribution"]}'\
-            + f'{rv_data["inputType"]}.model_validate({rv_data})'
+    string = (
+        f'quoFEM_RV_models.{rv_data["distribution"]}'
+        + f'{rv_data["inputType"]}.model_validate({rv_data})'
+    )
     rv = eval(string)
     return make_ERADist_object(name=rv.ERAName, opt=rv.ERAOpt, val=rv.ERAVal)
 
 
 def make_list_of_marginal_distributions(
-        list_of_random_variables_data) -> list[ERADist]:
+    list_of_random_variables_data,
+) -> list[ERADist]:
     marginal_ERAdistribution_objects_list = []
     for rv_data in list_of_random_variables_data:
         marginal_ERAdistribution_objects_list.append(
-            create_one_marginal_distribution(rv_data))
+            create_one_marginal_distribution(rv_data)
+        )
     return marginal_ERAdistribution_objects_list
 
 
@@ -269,47 +293,57 @@ def make_ERANataf_object(list_of_ERADist, correlation_matrix) -> ERANataf:
 
 
 class ERANatafJointDistribution:
-    def __init__(self, list_of_random_variables_data: list, 
-                 correlation_matrix_data: NDArray) -> None:
+    def __init__(
+        self,
+        list_of_random_variables_data: list,
+        correlation_matrix_data: NDArray,
+    ) -> None:
         self.list_of_random_variables_data = list_of_random_variables_data
         self.correlation_matrix_data = correlation_matrix_data
 
         self.num_rvs = len(self.list_of_random_variables_data)
         self.correlation_matrix = make_correlation_matrix(
-            self.correlation_matrix_data, self.num_rvs)
-        self.marginal_ERAdistribution_objects_list = \
+            self.correlation_matrix_data, self.num_rvs
+        )
+        self.marginal_ERAdistribution_objects_list = (
             make_list_of_marginal_distributions(
-                self.list_of_random_variables_data)
+                self.list_of_random_variables_data
+            )
+        )
         self.ERANataf_object = make_ERANataf_object(
-            self.marginal_ERAdistribution_objects_list, 
-            self.correlation_matrix)
+            self.marginal_ERAdistribution_objects_list, self.correlation_matrix
+        )
 
-    def u_to_x(self, u: NDArray, 
-               jacobian: bool=False) -> Union[tuple[NDArray[np.float64], Any], 
-                                              NDArray[np.float64]]:
-        return self.ERANataf_object.U2X(U=u, Jacobian=jacobian) 
-    
-    def x_to_u(self, x: NDArray, 
-               jacobian: bool=False) -> Union[tuple[NDArray[np.floating[Any]], 
-                                                    NDArray[np.floating[Any]]], 
-                                                    NDArray[np.floating[Any]]]:
+    def u_to_x(
+        self, u: NDArray, jacobian: bool = False
+    ) -> Union[tuple[NDArray[np.float64], Any], NDArray[np.float64]]:
+        return self.ERANataf_object.U2X(U=u, Jacobian=jacobian)
+
+    def x_to_u(
+        self, x: NDArray, jacobian: bool = False
+    ) -> Union[
+        tuple[NDArray[np.floating[Any]], NDArray[np.floating[Any]]],
+        NDArray[np.floating[Any]],
+    ]:
         return self.ERANataf_object.X2U(X=x, Jacobian=jacobian)
 
     def pdf(self, x: NDArray) -> Union[Any, NDArray[np.float64]]:
         return self.ERANataf_object.pdf(X=x)
-    
+
     def logpdf(self, x: NDArray) -> NDArray[np.float64]:
         return np.log(self.pdf(x))
-    
+
     def cdf(self, x: NDArray) -> float:
         return self.ERANataf_object.cdf(X=x)
-    
-    def random(self, list_of_rngs: list[np.random.Generator]=[], 
-               n: int=1) -> Union[tuple[NDArray[np.float64], Any], 
-                                  NDArray[np.float64]]:
+
+    def random(
+        self, list_of_rngs: list[np.random.Generator] = [], n: int = 1
+    ) -> Union[tuple[NDArray[np.float64], Any], NDArray[np.float64]]:
         if list_of_rngs == []:
-            list_of_rngs = [np.random.default_rng(seed=i) for i in range(len(
-                self.marginal_ERAdistribution_objects_list))]
+            list_of_rngs = [
+                np.random.default_rng(seed=i)
+                for i in range(len(self.marginal_ERAdistribution_objects_list))
+            ]
         u = np.zeros((n, len(list_of_rngs)))
         for i, rng in enumerate(list_of_rngs):
             u[:, i] = rng.normal(size=n)
