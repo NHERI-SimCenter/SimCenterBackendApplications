40696773217c17c17e49f79d98b5d8125c815611
Barbaros <bacetiner@ucla.edu>
2024-02-28 19:52:31 -0500

    bc - Corrected importlib and requests issues with R2D Windows Python & added string location queries

--- modules/tools/BRAILS/runBrails.py
+++ modules/tools/BRAILS/runBrails.py
@@ -1,18 +1,30 @@
 # Import packages needed for setting up required packages:
 import sys
 import subprocess
-import importlib.metadata
+from importlib import metadata as importlib_metadata
 
 # If not installed, install BRAILS, argparse, and requests:
 required = {'BRAILS', 'argparse', 'requests'}
 installed = set()
-for x in importlib.metadata.distributions():
+
+# Detect installed packages using Python-provided importlib.metadata:
+for x in importlib_metadata.distributions():
     try:
         installed.add(x.name)
     except:
         pass
+
+# If installed packages could not be detected, use importlib_metadata backport:
+if not installed:
+    import importlib_metadata
+    for x in importlib_metadata.distributions():
+        try:
+            installed.add(x.name)
+        except:
+            pass
 missing = required - installed
 
+# Install missing packages:
 python = sys.executable
 if missing:
     print('\nInstalling packages required for running this widget...')
@@ -20,15 +32,18 @@ if missing:
                           stdout=subprocess.DEVNULL)
     print('Successfully installed the required packages')
 
-# If BRAILS was previously installed ensure it is the latest version:
+# If requests and BRAILS were previously installed ensure they are at their latest versions:
+subprocess.check_call([python, '-m', 'pip', 'install', 'requests','-U'],
+                      stdout=subprocess.DEVNULL)
+ 
 import requests
 latestBrailsVersion = requests.get('https://pypi.org/pypi/BRAILS/json').json()['info']['version']
-if  importlib.metadata.version('BRAILS')!=latestBrailsVersion:
+if  importlib_metadata.version('BRAILS')!=latestBrailsVersion:
     print('\nAn older version of BRAILS was detected. Updating to the latest BRAILS version..')    
     subprocess.check_call([python, '-m', 'pip', 'install', 'BRAILS','-U'],
                           stdout=subprocess.DEVNULL)
     print('Successfully installed the latest version of BRAILS')   
-    
+ 
 # Import packages required for running the latest version of BRAILS:
 import argparse
 import os
@@ -41,13 +56,19 @@ def log_msg(msg):
     print(formatted_msg)
 
 # Define a way to call BRAILS InventoryGenerator:
-def runBrails(latMin, latMax, longMin, longMax,
-              seed, numBuildings, gKey, outputFile, lengthUnit):    
+def runBrails(latMin, latMax, longMin, longMax, locationStr, fpSource,
+              fpAttrMap, seed, numBuildings, gKey, outputFile, lengthUnit):    
     
     # Initialize InventoryGenerator:
-    invGenerator = InventoryGenerator(location=(longMin,latMin,longMax,latMax),
-                                      nbldgs=numBuildings, randomSelection=seed,
-                                      GoogleAPIKey=gKey)
+    if locationStr=="":
+        invGenerator = InventoryGenerator(location=(longMin,latMin,longMax,latMax),
+                                          nbldgs=numBuildings, randomSelection=seed,
+                                          GoogleAPIKey=gKey)
+    else:
+        invGenerator = InventoryGenerator(location=locationStr,
+                                          nbldgs=numBuildings, randomSelection=seed,
+                                          GoogleAPIKey=gKey)
+    
 
     # Run InventoryGenerator to generate an inventory for the entered location:
     invGenerator.generate(attributes='all', outFile=outputFile, 
@@ -60,6 +81,9 @@ def main(args):
     parser.add_argument('--latMax', default=None, type=float)
     parser.add_argument('--longMin', default=None, type=float)
     parser.add_argument('--longMax', default=None, type=float)
+    parser.add_argument('--location', default=None, type=str)
+    parser.add_argument('--fpSource', default=None, type=str)
+    parser.add_argument('--fpAttrMap', default=None, type=str)
     parser.add_argument('--outputFile', default=None)
     parser.add_argument('--googKey', default=None)
     parser.add_argument('--seed', default=None, type=int)
@@ -74,9 +98,9 @@ def main(args):
 
     # Run BRAILS BRAILS InventoryGenerator with the user-defined arguments:
     runBrails(
-        args.latMin, args.latMax, args.longMin, args.longMax,
-        args.seed, args.numBuildings, args.googKey, args.outputFile,
-        args.lengthUnit)
+        args.latMin, args.latMax, args.longMin, args.longMax, args.location,
+        args.fpSource, args.fpAttrMap, args.seed, args.numBuildings, args.googKey, 
+        args.outputFile, args.lengthUnit)
 
     log_msg('BRAILS successfully generated the requested building inventory')
     
