85ed9527a3812ceeee82c32c9e9fe7c7a2687007
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-08-18 10:31:20 -0700

    Awesome. Remove typo.
    Remove typo. We are done here.

--- modules/performUQ/common/parallel_runner_mpi4py.py
+++ modules/performUQ/common/parallel_runner_mpi4py.py
@@ -1,8 +1,6 @@
 from mpi4py import MPI  # noqa: INP001, D100
 from mpi4py.futures import MPIPoolExecutor
 
-# aceleration should be acceleration
-
 class ParallelRunnerMPI4PY:  # noqa: D101
     def __init__(self, run_type: str = 'runningRemote') -> None:
         self.run_type = run_type
