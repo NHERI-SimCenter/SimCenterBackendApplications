e1e6ef9cd95b5394523d421619d62f3006e463aa
Sina Naeimi <snaeimi@udel.edu>
2024-08-26 14:36:00 -0700

    Modified REWET-PyReCoDes interface. Tested

--- modules/performREC/pyrecodes/rewet_helper.py
+++
@@ -1,9 +0,0 @@
-import rewet.Input.Settings
-
-
-def get_rewet_hydraulic_basic_setting():
-    settings = rewet.Input.Settings.Settings()
-    settings_dict = settings.process.settings
-    # settings_dict.update(settings.scenario.settings)
-
-    return settings_dict
