e0dd9966fbbe6bc6b88c05a52f35475f7d25ab69
Justin Bonus <bonus@berkeley.edu>
2025-01-19 10:50:56 -0700

    Update Celeris setrun for temp examples pathway, will be changed to user-input and hosted within app repos (e.g. HydroUQ/Examples) later

--- modules/createEVENT/Celeris/setrun.py
+++ modules/createEVENT/Celeris/setrun.py
@@ -8,15 +8,15 @@ ti.init(arch = ti.gpu, advanced_optimization = True, kernel_profiler = False)
 precision =ti.f32 # ti.f16 for half-precision or ti.f32 for single precision
 
 # 1) Set the topography data
-baty = Topodata(datatype='celeris',path='./examples/CrescentCity',filename='bathy.txt')
+baty = Topodata(datatype='celeris',path='./applications/createEVENT/Celeris/examples/CrescentCity',filename='bathy.txt')
 
 # 2) Set Boundary conditions
-bc = BoundaryConditions(celeris=True,path='./examples/CrescentCity',precision=precision)
+bc = BoundaryConditions(celeris=True,path='./applications/createEVENT/Celeris/examples/CrescentCity',precision=precision)
 
 # 3) Build Numerical Domain
 d = Domain(topodata=baty,precision=precision)
 
-# 4) Solve using SWE BOUSS
+# 4) Solve using SWE (0) BOUSS (1)
 solver = Solver(domain=d, boundary_conditions=bc)
 
 # 5) Execution
@@ -25,11 +25,11 @@ solver = Solver(domain=d, boundary_conditions=bc)
 # run = None
 # time.sleep(1)
 # print("Warm start")
-run = Evolve(solver = solver, maxsteps= 100000, saveimg=True)
+run = Evolve(solver = solver, maxsteps= 100000, saveimg=False,)
 
 # run.Evolve_Headless() # Faster , no visualization
 
 # Showing -> 'h'    # cmap any on matplotlib Library e.g. 'BuGn', 'Blues'
-# run.Evolve_Display(variable='h',cmapWater='Blues', showSediment=True)
-run.Evolve_Display(variable='eta',vmin=-5,vmax=5,cmapWater='Blues', showSediment=True)
+run.Evolve_Display(variable='h',cmapWater='Blues', showSediment=True)
+# run.Evolve_Display(variable='eta',vmin=-5,vmax=5,cmapWater='Blues', showSediment=True)
 # run.Evolve_Display(variable='vor',vmin=-0.5,vmax=0.5,cmapWater='viridis')
