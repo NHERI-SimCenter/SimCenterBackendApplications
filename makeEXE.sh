b4baaedcca708adad41b0f45d5c129f1ed622303
Justin Bonus <bonus@berkeley.edu>
2025-01-20 22:06:13 -0700

    Testing openblas through conan, test libomp include and linking for mac (may be better to brew install or hardcode typical header path for mac instead of <omp.h> )

--- makeEXE.sh
+++ makeEXE.sh
@@ -5,9 +5,9 @@ mkdir -p build
 cd build
 conan install .. --build missing
 status=$?; if [[ $status != 0 ]]; then echo "conan failed"; exit $status; fi
-cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF -L/usr/local/opt/libomp/lib -I/usr/local/opt/libomp/include ..
+cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF ..
 status=$?; if [[ $status != 0 ]]; then echo "cmake failed"; exit $status; fi
-cmake --build . --config Release
+cmake --build -L/usr/local/opt/libomp/lib -I/usr/local/opt/libomp/include . --config Release
 status=$?; if [[ $status != 0 ]]; then echo "make failed"; exit $status; fi
 cmake --install .
 status=$?; if [[ $status != 0 ]]; then echo "make install failed"; exit $status; fi
