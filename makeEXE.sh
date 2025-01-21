6b72bfe79c5f2948e226063555995ee0c6cc2f33
Justin Bonus <bonus@berkeley.edu>
2025-01-21 11:48:16 -0700

    appveyor: Assume typical libomp include and library path for macOS, see if it resolves <omp.h>

--- makeEXE.sh
+++ makeEXE.sh
@@ -5,7 +5,7 @@ mkdir -p build
 cd build
 conan install .. --build missing
 status=$?; if [[ $status != 0 ]]; then echo "conan failed"; exit $status; fi
-cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF .. -DCMAKE_LIBRARY_PATH="/usr/local/opt/libomp/lib" -DCMAKE_INCLUDE_PATH="/usr/local/opt/libomp/include"
+cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_LIBRARY_PATH="/usr/local/opt/libomp/lib" -DCMAKE_INCLUDE_PATH="/usr/local/opt/libomp/include" .. 
 status=$?; if [[ $status != 0 ]]; then echo "cmake failed"; exit $status; fi
 cmake --build . --config Release
 status=$?; if [[ $status != 0 ]]; then echo "make failed"; exit $status; fi
