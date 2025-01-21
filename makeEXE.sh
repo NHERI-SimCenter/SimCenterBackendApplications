8640303ac252a4fdb9d3faa692634ba51080b081
Justin Bonus <bonus@berkeley.edu>
2025-01-21 00:03:41 -0700

    appveyor: LAPACK cmake library flag for windows and use different include/library flag style for cmake configure on mac and Ubuntu

--- makeEXE.sh
+++ makeEXE.sh
@@ -5,9 +5,9 @@ mkdir -p build
 cd build
 conan install .. --build missing
 status=$?; if [[ $status != 0 ]]; then echo "conan failed"; exit $status; fi
-cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF ..
+cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF .. -DCMAKE_LIBRARY_PATH="/usr/local/opt/libomp/lib" -DCMAKE_INCLUDE_PATH="/usr/local/opt/libomp/include"
 status=$?; if [[ $status != 0 ]]; then echo "cmake failed"; exit $status; fi
-cmake --build -L/usr/local/opt/libomp/lib -I/usr/local/opt/libomp/include . --config Release
+cmake --build . --config Release
 status=$?; if [[ $status != 0 ]]; then echo "make failed"; exit $status; fi
 cmake --install .
 status=$?; if [[ $status != 0 ]]; then echo "make install failed"; exit $status; fi
