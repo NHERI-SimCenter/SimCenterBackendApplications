3058bffa5692a55ace61deaaa48f28aebdc79aee
fmk <fmckenna@berkeley.edu>
2026-04-26 22:51:36 -0700

    fmk - moving to traditional CMAkeLists files for better readability and integration with IDE's of applications in SimCenterBAckend

--- conanfile.py
+++ conanfile.py
@@ -1,71 +1,54 @@
 import os  # noqa: D100
 
-from conans import CMake, ConanFile
+from conan import ConanFile
+from conan.tools.cmake import CMake
+from conan.tools.files import copy
 
 
 class simCenterBackendApps(ConanFile):  # noqa: D101
-    name = 'SimCenterBackendApplications'
-    version = '1.2.2'
-    description = 'Backend applications for SimCenter software'
-    license = 'BSD 3-Clause'
-    author = 'Michael Gardner mhgardner@berkeley.edu'
-    url = 'https://github.com/NHERI-SimCenter/SimCenterBackendApplications'
-    settings = {  # noqa: RUF012
-        'os': None,
-        'build_type': None,
-        'compiler': None,
-        'arch': ['x86_64', 'armv8'],
-    }
-    options = {'shared': [True, False]}  # noqa: RUF012
+    name = "SimCenterBackendApplications"
+    version = "1.2.2"
+    description = "Backend applications for SimCenter software"
+    license = "BSD 3-Clause"
+    author = "Michael Gardner mhgardner@berkeley.edu"
+    url = "https://github.com/NHERI-SimCenter/SimCenterBackendApplications"
+    settings = "os", "compiler", "build_type", "arch"
+    package_type = "application"
+    short_paths = True
+
     default_options = {  # noqa: RUF012
-        'libcurl:with_ssl': 'openssl',
-        'boost:without_fiber': True,
+        "libcurl/*:with_ssl": "openssl",
+        "boost/*:without_fiber": True,
     }
-    generators = 'cmake'
-    build_policy = 'missing'
+
+    generators = "CMakeToolchain", "CMakeDeps"
+    build_policy = "missing"
+
     requires = [  # noqa: RUF012
-        'jansson/2.13.1',
-        'zlib/1.3.1',
-        'libcurl/8.4.0',
-        'eigen/3.3.7',
-        'clara/1.1.5',
-        'jsonformoderncpp/3.7.0',
-        'nanoflann/1.3.2',
-        'nlopt/2.7.1',
-        'kissfft/131.1.0',
-        'boost/1.84.0'        
+        "jansson/2.14",
+        "zlib/1.3.1",
+        "libcurl/8.12.1",
+        "eigen/3.4.0",
+        "clara/1.1.5",
+        "nanoflann/1.6.0",
+        "nlopt/2.10.0",
+        "kissfft/131.1.0",
+        "boost/1.88.0",
+        "nlohmann_json/3.11.3",
     ]
 
-
-    # Custom attributes for Bincrafters recipe conventions
-    _source_subfolder = 'source_subfolder'
-    _build_subfolder = 'build_subfolder'
-    # Set short paths for Windows
-    short_paths = True
-    scm = {  # noqa: RUF012
-        'type': 'git',  # Use "type": "svn", if local repo is managed using SVN
-        'subfolder': _source_subfolder,
-        'url': 'auto',
-        'revision': 'auto',
-    }
-
-    def configure(self):  # noqa: D102
-        self.options.shared = False
-
-    def configure_cmake(self):  # noqa: D102
-        cmake = CMake(self)
-        cmake.configure(source_folder=self._source_subfolder)
-        return cmake
-
     def build(self):  # noqa: D102
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):  # noqa: D102
-        self.copy(pattern='LICENSE', dst='licenses', src=self._source_subfolder)
-        cmake = self.configure_cmake()
+        copy(self, "LICENSE", self.source_folder,
+             os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
         cmake.install()
-        self.copy('*', dst='bin', src=self._source_subfolder + '/applications')
+        copy(self, "*", os.path.join(self.source_folder, "applications"),
+             os.path.join(self.package_folder, "bin"))
 
     def package_info(self):  # noqa: D102
-        self.env_info.PATH.append(os.path.join(self.package_folder, 'bin'))  # noqa: PTH118
+        self.cpp_info.bindirs = ["bin"]
--- makeMac.sh
+++ makeMac.sh
@@ -1,24 +1,12 @@
 
-# NOTE if fail in nataf_gsa on Mac it is because clang still not working with omp, you need a brew install of libomp, issue:
-#     brew install libomp
+# NOTE if fails in nataf_gsa on Mac it is because clang still not working with omp,
+# SOLN: you need a brew install of libomp, issue:
+#           brew install libomp
 
 # define the compilers
 export CC=clang
 export CXX=clang++
 export CMAKE_ARGS=-DCMAKE_POLICY_VERSION_MINIMUM=3.5
-
-
-#
-# following is temp until conan2 fully tested
-#
-
-cp conanfile.py conanfile.py.ORIG
-cp CMakeLists.txt CMakeLists.txt.ORIG
-cp cmake/SimCenterFunctions.cmake ./cmake/SimCenterFunctions.cmake.ORIG
-
-cp conanfile2.py conanfile.py
-cp CMakeLists2.txt CMakeLists.txt
-cp cmake/SimCenterFunctions2.cmake ./cmake/SimCenterFunctions.cmake
 
 #
 # run conan to install dependencies
@@ -35,28 +23,23 @@ cmake --build build
 cmake --install build --prefix ./applications
 
 #
-# Bundle libomp.dylib with nataf_gsa so it runs without needing a brew install on user machine
+# Bundle libomp.dylib with nataf_gsa and run install_name_tool on natf_gsa so it runs without needing a brew install on user machine
 #
 
-NATAF_DIR="applications/performUQ/SimCenterUQ"
+NATAF_DIR="${PWD}/applications/performUQ/SimCenterUQ"
 LIBOMP_DIR="$(brew --prefix libomp)/lib"
 LIBOMP_SRC="${LIBOMP_DIR}/libomp.dylib"
+echo "$PWD ${LIBOMP_SRC} ${NATAF_DIR}/libomp.dylib "
+
 
 if [ -f "${NATAF_DIR}/nataf_gsa" ] && [ -f "${LIBOMP_SRC}" ]; then
-    cp "${LIBOMP_SRC}" "${NATAF_DIR}/libomp.dylib"
+    cp "${LIBOMP_SRC}" "${NATAF_DIR}"
     install_name_tool -change "${LIBOMP_SRC}" "@executable_path/libomp.dylib" "${NATAF_DIR}/nataf_gsa"
     echo "Bundled libomp.dylib into ${NATAF_DIR}"
 else
     echo "WARNING: nataf_gsa or libomp.dylib not found — skipping libomp bundling"
 fi
 
-#
-# again temp until migration to conan2 complete
-#
-
-mv conanfile.py.ORIG conanfile.py
-mv CMakeLists.txt.ORIG CMakeLists.txt
-mv cmake/SimCenterFunctions.cmake.ORIG ./cmake/SimCenterFunctions.cmake
 
 
 
